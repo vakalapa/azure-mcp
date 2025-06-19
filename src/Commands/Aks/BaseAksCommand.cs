8d9d4a8b55144b3acb2a421138668c3fbe06fd1b
Vamsi Kalapala <vakr@microsoft.com>
2025-06-19 16:32:07 -0700

    removing the existing RG option

--- src/Commands/Aks/BaseAksCommand.cs
+++ src/Commands/Aks/BaseAksCommand.cs
@@ -9,44 +9,28 @@ public abstract class BaseAksCommand<
     [DynamicallyAccessedMembers(TrimAnnotations.CommandAnnotations)] TOptions>
     : SubscriptionCommand<TOptions> where TOptions : BaseAksOptions, new()
 {
-    protected new readonly Option<string> _resourceGroupOption = OptionDefinitions.Common.ResourceGroup;
     protected readonly Option<string> _clusterOption = OptionDefinitions.Aks.Cluster;
 
     // Allow derived classes to specify if resource group is required
-    protected virtual bool RequiresResourceGroup => true;
-
-    protected override void RegisterOptions(Command command)
+    protected virtual bool RequiresResourceGroup => true;    protected override void RegisterOptions(Command command)
     {
         base.RegisterOptions(command);
 
-        // Create a copy of the resource group option with the required setting from derived class
-        var resourceGroupOption = new Option<string>(
-            _resourceGroupOption.Name,
-            _resourceGroupOption.Description!)
-        {
-            IsRequired = RequiresResourceGroup
-        };
-        command.AddOption(resourceGroupOption);
-
+        // Add resource group option and set if it's required
+        _resourceGroupOption.IsRequired = RequiresResourceGroup;
+        command.AddOption(_resourceGroupOption);
+        
         // Only add cluster option for commands that need it (BaseClusterOptions or derived)
         if (typeof(TOptions).IsAssignableTo(typeof(Options.Aks.Cluster.BaseClusterOptions)))
         {
             command.AddOption(_clusterOption);
         }
-    }
-
-    protected override TOptions BindOptions(ParseResult parseResult)
+    }    protected override TOptions BindOptions(ParseResult parseResult)
     {
         var options = base.BindOptions(parseResult);
 
-        // Find the resource group option that was actually added to the command
-        var resourceGroupOption = parseResult.CommandResult.Command.Options
-            .FirstOrDefault(o => o.Name == _resourceGroupOption.Name) as Option<string>;
-
-        if (resourceGroupOption != null)
-        {
-            options.ResourceGroup = parseResult.GetValueForOption(resourceGroupOption);
-        }
+        // Bind resource group option
+        options.ResourceGroup = parseResult.GetValueForOption(_resourceGroupOption);
 
         // Only bind cluster-specific options for commands that need them
         if (options is Options.Aks.Cluster.BaseClusterOptions clusterOptions)
